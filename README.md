339e25c31fc3628a0edd1c2d9db5aece9feb0288
kewwL <zxcat174@gmail.com>
2024-12-21 14:51:57 +0500

    init
    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# rp2040-basic-firmware
---
+++ test/firmware.s
@@ -0,0 +1,9 @@
+//       rp2040-basic-firmware by keww:L
+//       All copyrights are protected by the MIT license: keww:L (2024)
+
+//       This code is part of the project `rp2040-basic-firmware`, and is also the main part of the firmware for rp2040, please do not modify this file, since it plays the role of the main firmware of the rp2040 microcontroller, and any interference, change and deletion of parts of the code can lead to unpredictable consequences, and can even damage the equipment. 
+//       Also, use the firmware and open source code from the 'main' branch, since the firmware uploaded to this branch is tested and suitable for use.
+
+//       Also, this software is provided "as is" and the author is not responsible for any consequences during the use of this software, including breakdown and other unpredictable consequences.
+
+
